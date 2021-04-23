771b696e140130bb75169b8b65752a1b98fff01d
Jose Mario Reisswitz <jmreisswitz@gmail.com>
2021-04-23 00:40:33 -0300

    add spring on django place

---
+++ backend/tattooManager/src/main/kotlin/com/tattoomanager/tattooManager/TattooManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.tattoomanager.tattooManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TattooManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TattooManagerApplication>(*args)
+}
---
+++ backend/tattooManager/src/test/kotlin/com/tattoomanager/tattooManager/TattooManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tattoomanager.tattooManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TattooManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
