36e288816e26128d1d4d5c355a2bd5148e91ec7a
Jose Mario Reisswitz <jmreisswitz@gmail.com>
2021-04-26 16:04:02 -0300

    add postgres depencendy and connection properties

--- backend/tattooManager/build.gradle.kts
+++ backend/tattooManager/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
