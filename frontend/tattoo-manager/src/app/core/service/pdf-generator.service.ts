e09750de45de563268c94a603bf0c07b4f3e75f0
Jose Mario Reisswitz <jmreisswitz@gmail.com>
2021-05-13 19:47:45 -0300

    removes commented code

--- frontend/tattoo-manager/src/app/core/service/pdf-generator.service.ts
+++ frontend/tattoo-manager/src/app/core/service/pdf-generator.service.ts
@@ -1,21 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Anamnese } from '../model/anamnese';
 import labelmake from 'labelmake';
+// @ts-ignore
 import templateJson from '../../../templates/anamnese-template.json';
-import TemplateType from 'labelmake';
-import { TemplateSchema } from 'labelmake/dist/types/type';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PdfGeneratorService {
   constructor() {}
-
-  //   export interface Template {
-  //   schemas: { [key: string]: TemplateSchema }[];
-  //   basePdf: PageSize | string | Uint8Array | ArrayBuffer;
-  //   fontName?: string;
-  // }
 
   async generateAnamnesePdf(anamnese: Anamnese): Promise<any> {
     const inputs = [
@@ -26,26 +19,9 @@ export class PdfGeneratorService {
     ];
     const font = {};
     const template = {
-      // {
-      schemas:
-        //   field1: {
-        //     position: { x: 20, y: 20 },
-        //     width: 50,
-        //     height: 50,
-        //     // fontSize: 30,
-        //     type: 'text',
-        //   },
-        // } as const,
-        templateJson.schemas.map((schemas: any) => {
-          // console.log('count: ' + count);
-          // count++;
-          // console.log(schemas);
-          // for (const [key] of Object.entries(schemas)) {
-          //   // schemas[key].type = TemplateType(schemas[key].type);
-          //   // schemas[key] = schemas[key] as const;
-          // }
-          return { schemas } as const;
-        }),
+      schemas: templateJson.schemas.map((schemas: any) => {
+        return { schemas } as const;
+      }),
       basePdf: templateJson.basePdf,
     };
     console.log(template);
