3749037e2a98d61e75a198e0f6420982f959290d
Jose Mario Reisswitz <jmreisswitz@gmail.com>
2021-05-16 16:59:14 -0300

    bind form to anamnese dto

--- frontend/tattoo-manager/src/app/modules/components/new-anamnese/new-anamnese.component.ts
+++ frontend/tattoo-manager/src/app/modules/components/new-anamnese/new-anamnese.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Anamnese } from '../../../core/model/anamnese';
 
 @Component({
   selector: 'app-new-anamnese',
   templateUrl: './new-anamnese.component.html',
-  styleUrls: ['./new-anamnese.component.css']
+  styleUrls: ['./new-anamnese.component.css'],
 })
 export class NewAnamneseComponent implements OnInit {
+  anamnese: Anamnese = Anamnese.getEmptyAnamnese();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  generateAnamnese(): void {
+    console.log(this.anamnese);
+    console.log(this.anamnese.clinicalInfo.cardiacProblem);
   }
-
 }
