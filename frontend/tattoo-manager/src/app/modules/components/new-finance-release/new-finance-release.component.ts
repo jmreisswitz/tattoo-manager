318a9199a777c0dd383a380b047a3ad69a8a7ac7
José Mário Reisswitz <jmreisswitz@gmail.com>
2021-05-16 23:02:23 -0300

    new release data binding on form

--- frontend/tattoo-manager/src/app/modules/components/new-finance-release/new-finance-release.component.ts
+++ frontend/tattoo-manager/src/app/modules/components/new-finance-release/new-finance-release.component.ts
@@ -8,6 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewFinanceReleaseComponent implements OnInit {
   // constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  expensesGroups = [
+    'Infraestrutura',
+    'Material',
+    'Marketing',
+    'Softwares',
+    'Desenvolvimento pessoal',
+  ];
+  revenueGroups = ['Tatuagem', 'Venda de desenhos', 'Cursos', 'Consultoria'];
+  releaseGroups = this.expensesGroups;
+
+  releaseTypes = ['Despesa', 'Receita'];
+
+  typeSelected: string | null = null;
+  groupSelected: string | null = null;
+  description: string | null = null;
+  value: number | null = null;
+
   constructor() {}
   ngOnInit(): void {}
+
+  changeType(releaseType: string): void {
+    this.typeSelected = releaseType;
+    if (this.typeSelected === 'Despesa') {
+      this.releaseGroups = this.expensesGroups;
+    } else {
+      this.releaseGroups = this.revenueGroups;
+    }
+  }
+
+  changeGroup(releaseGroup: string): void {
+    this.groupSelected = releaseGroup;
+  }
+
+  saveFinanceRelease(): void {}
 }
